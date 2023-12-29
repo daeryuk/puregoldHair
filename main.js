cc1dda57f8e5c7edfe979b0454dc59ebea94615f
daeryuk <eofbrdlehdtjf@naver.com>
2023-12-29 22:07:56 +0900

    footer 수정

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // main.js
+window.onload = function () {
+    // 세션 스토리지에서 사용자의 이름을 가져옵니다.
+    var name = sessionStorage.getItem('username') || 'Anonymous';
 
+    // 사용자의 이름을 화면에 출력합니다.
+    var welcomeMessage = document.getElementById('welcomeMessage');
+    welcomeMessage.textContent = '환영합니다. ' + name + '님! ';
+
+};
